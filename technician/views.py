dab26c75b04538c06fa9ce60d120881209decaef
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 01:22:20 +0530

    added api endpoints to update the repair status as well as accept the repair request

--- technician/views.py
+++ technician/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from .serializer import SentRequestSerializer
+from .serializer import SentRequestSerializer, RepairSerializer
 from .models import SentRequest, Repair
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -20,3 +20,27 @@ class AllRequestListAPI(APIView):
         response=paginator.get_paginated_response(serial.data)
         cache.set(key, response.data, timeout=300)
         return response
+
+class RepairRequestAcceptAPI(APIView):
+    def patch(self, request, pk):
+        instance=get_object_or_404(SentRequest.objects.select_related('technician__user', 'request'), is_accepted=False, id=pk)
+        serial=SentRequestSerializer(instance, data=request.data, partial=True)
+        if serial.is_valid():
+            serial.save()
+            Repair.objects.create(accepted_request=instance)
+            return Response(serial.data, status=200)
+        return Response(serial.errors, status=400)
+
+    def delete(self, request, pk):
+        instance=get_object_or_404(SentRequest.objects.select_related('technician__user', 'request'), is_accepted=False, id=pk)
+        instance.delete()
+        return Response(status=204)
+
+class RepairStatusUpdateAPI(APIView):
+    def patch(self, request, pk):
+        instance=get_object_or_404(Repair.objects.select_related('accepted_request'), accepted_request__is_accepted=True, id=pk)
+        serial=RepairSerializer(instance, data=request.data, partial=True)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data, status=200)
+        return Response(serial.errors, status=400)
