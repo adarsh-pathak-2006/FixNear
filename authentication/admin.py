014c8ed9896ba721e7842196fc519d8f01aa438f
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-03 17:44:56 +0530

    added models in the authentication app

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TechnicianProfile, CustomerProfile
 
-# Register your models here.
+admin.site.register(TechnicianProfile)
+admin.site.register(CustomerProfile)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES=[('TECHNICIAN', 'Technician'), ('CUSTOMER', 'Customer'), ('ADMIN', 'Admin')]
+
+    mobile_no=models.CharField(max_length=15, unique=True)
+    role=models.CharField(max_length=10, choices=ROLE_CHOICES, default='CUSTOMER')
+
+class CustomerProfile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    latitute=models.CharField(max_length=20, null=True)
+    longitude=models.CharField(max_length=20, null=True)
+    is_verified=models.BooleanField(default=False)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+class TechnicianProfile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    bio=models.TextField(null=True)
+    experience=models.PositiveIntegerField(null=True)
+    average_rating=models.IntegerField(null=True)
+    total_job=models.PositiveIntegerField(default=0)
+    is_avaliable=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
