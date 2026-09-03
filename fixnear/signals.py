8a1b77521dd28949dd60ba8409dcdbcd087be011
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 00:40:04 +0530

    added signals to automatically create entry in the sentRequest model and send the notifications to the specific technicians

--- fixnear/signals.py
+++ fixnear/signals.py
@@ -4,6 +4,8 @@ from django.contrib.auth import get_user_model
 from authentication.models import CustomerProfile, TechnicianProfile
 from fixnear.cache_key import customer_profile_key, technician_profile_key, technicianlist_key
 from django.core.cache import cache
+from customer.models import RepairRequest
+from technician.models import SentRequest
 
 User=get_user_model()
 
@@ -23,3 +25,10 @@ def TechnicianProfileCacheInvalidation(sender, instance, created, **kwargs):
     cache.delete(technician_profile_key(instance.user.id))
     for i in range(1, 100):
         cache.delete(technicianlist_key(i))
+
+@receiver(post_save, sender=RepairRequest)
+def SendRequestToTheAppropiateTechnicians(sender, instance, created, **kwargs):
+    if created:
+        profiles=TechnicianProfile.objects.select_related('user').filter(skill=instance.skills_required)
+        for profile in profiles:
+            SentRequest.objects.create(technician=profile, request=instance)
