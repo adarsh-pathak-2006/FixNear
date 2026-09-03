b53ea5e58600da26a8ca7035272a2f91ba8f4729
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 01:00:21 +0530

    added api endpoint to list all the recieved requests

--- fixnear/cache_key.py
+++ fixnear/cache_key.py
@@ -5,4 +5,7 @@ def technician_profile_key(pk):
     return f"technician_profile_key_userID:{pk}"
 
 def technicianlist_key(page_no):
-    return f"techniciam_list_key_pageNo:{page_no}"
+    return f"technician_list_key_pageNo:{page_no}"
+
+def repairrequest_list_key(userid, page_no):
+    return f"repairrequest_list_on_userid:{userid}_pageno:{page_no}"
--- technician/views.py
+++ technician/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .serializer import SentRequestSerializer
+from .models import SentRequest, Repair
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from fixnear.pagination import GeneralPagnination
+from fixnear.cache_key import repairrequest_list_key
+from django.core.cache import cache
 
-# Create your views here.
+class AllRequestListAPI(APIView):
+    def get(self, request):
+        pageno=request.query_params.get("page", "1")
+        key=repairrequest_list_key(page_no=pageno, userid=request.user.id)
+        cached_data=cache.get(key)
+        if cached_data:
+            return Response(cached_data, status=200)
+        paginator=GeneralPagnination()
+        data=paginator.paginate_queryset(SentRequest.objects.select_related('technician__user', 'request').filter(technician__user=request.user), request, view=self)
+        serial=SentRequestSerializer(data, many=True)
+        response=paginator.get_paginated_response(serial.data)
+        cache.set(key, response.data, timeout=300)
+        return response
