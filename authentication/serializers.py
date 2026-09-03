300e52372a38e6e68ac02502af47e658e1556a16
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-03 21:16:08 +0530

    added serializers in the authentication app

---
+++ authentication/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from .models import CustomerProfile, TechnicianProfile, TechnicalSkill
+
+User=get_user_model()
+
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model=User
+        fields=['username', 'email', 'mobile_no', 'role', 'password']
+        write_only_fields=['password']
+
+class UserGetSerializer(ModelSerializer):
+    class Meta:
+        model=User
+        fields=['username', 'email', 'role']
+
+class CustomerSerializer(ModelSerializer):
+    user=UserGetSerializer(read_only=True)
+    class Meta:
+        model=CustomerProfile
+        fields='__all__'
+        read_only_fields=['created_on']
+
+
+class TechnicianProfileSerializer(ModelSerializer):
+    class Meta:
+        model=TechnicianProfile
+        fields='__all__'
+        read_only_fields=['created_on']
+
+class TechnicalSkillSerializer(ModelSerializer):
+    user=TechnicianProfileSerializer(read_only=True)
+    class Meta:
+        model=TechnicalSkill
+        fields='__all__'
