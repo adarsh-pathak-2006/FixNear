a78001c9ece003897ecd55a953204ee913615665
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-03 17:55:40 +0530

    added skill model and wired it on the adminpanel

--- authentication/models.py
+++ authentication/models.py
@@ -27,3 +27,12 @@ class TechnicianProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class TechnicalSkill(models.Model):
+    user=models.ForeignKey(TechnicianProfile, on_delete=models.CASCADE)
+    skill_name=models.CharField(max_length=50)
+    experience_level=models.PositiveIntegerField(max_length=5)
+    description=models.TextField(null=True)
+
+    def __str__(self):
+        return self.skill_name
