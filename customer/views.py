d3ee6e2b464a497fc077071777eed0e54c722afa
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 01:39:35 +0530

    added permission and throttling in customer app

--- customer/views.py
+++ customer/views.py
@@ -3,8 +3,12 @@ from .serializer import RepairRequestSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from authentication.models import CustomerProfile
+from fixnear.throttling import GeneralThrottle
+from fixnear.permissions import IsCustomer
 
 class RepairRequestAPI(APIView):
+    permission_classes=[IsCustomer]
+    throttle_classes=[GeneralThrottle]
     def post(self, request):
         serial=RepairRequestSerializer(data=request.data)
         if serial.is_valid():
