beeff4b12f12ba091be2af0a0b4da77ec29fa067
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 21:34:23 +0530

    switched back to wsgi architecture

--- fixnear/asgi.py
+++ fixnear/asgi.py
@@ -13,13 +13,5 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fixnear.settings')
 
-asgi_application = get_asgi_application()
+application = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from core.routing import websocket_urlpatterns
-
-application=ProtocolTypeRouter({
-    'http':asgi_application,
-    'websocket':AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-})
--- fixnear/settings.py
+++ fixnear/settings.py
@@ -70,8 +70,8 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'fixnear.wsgi.application'
-ASGI_APPLICATION = 'fixnear.asgi.application'
+WSGI_APPLICATION = 'fixnear.wsgi.application'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
