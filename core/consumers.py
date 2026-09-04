72326cd06b40ecc8d4f478caf75f834e7e958676
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 20:46:01 +0530

    created log model too

--- core/consumers.py
+++ core/consumers.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404
 from channels.generic.websocket import AsyncWebsocketConsumer
 from technician.models import SentRequest
+from .models import NotificationLog
+from authentication.models import TechnicianProfile
 import json
 
 class TaskNotificationConsumer(AsyncWebsocketConsumer):
@@ -16,6 +19,8 @@ class TaskNotificationConsumer(AsyncWebsocketConsumer):
         db_data=await SentRequest.objects.select_related('technician__user').aget(technician__user=self.user)
         message_skill=db_data.request.skills_required
         message_requirement=db_data.request.requirement
+        tech_profile_data=await TechnicianProfile.objects.select_related('user').aget(user=self.user)
+        await NotificationLog.objects.acreate(technician=tech_profile_data, skill_required=message_skill, requirement=message_requirement)
         await self.channel_layer.group_send(self.group_name, {'type':'notification', 'notification_skill_required':message_skill, 'notification_requirement':message_requirement})
 
     async def notification(self, event):
