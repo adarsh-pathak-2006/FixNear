f9d832bf184e1df9f786d527c23dffd0933d7eb2
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 20:39:12 +0530

    addded the model and also wired it on the admin panel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from authentication.models import TechnicianProfile
 
-# Create your models here.
+class NotificationLog(models.Model):
+    technician=models.ForeignKey(TechnicianProfile, on_delete=models.CASCADE)
+    skill_required=models.CharField(max_length=100)
+    requirement=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.technician.user.username
+
