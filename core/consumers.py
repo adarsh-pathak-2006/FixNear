4f742ffdb42ce9330b23b5f60afef0b2d1ce49e4
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-04 20:35:01 +0530

    added routing for consumer to work

--- core/consumers.py
+++ core/consumers.py
@@ -1,7 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from technician.models import SentRequest
 import json
-from asgiref.sync import sync_to_async
 
 class TaskNotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -20,7 +19,7 @@ class TaskNotificationConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(self.group_name, {'type':'notification', 'notification_skill_required':message_skill, 'notification_requirement':message_requirement})
 
     async def notification(self, event):
-        await self.send(text_data=json.dumps({'notification_skill_required':event['notification_message'], 'notification_requirement':event.get('notification_requirement')}))
+        await self.send(text_data=json.dumps({'notification_skill_required':event['notification_skill_required'], 'notification_requirement':event.get('notification_requirement')}))
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
--- core/routing.py
+++ core/routing.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .consumers import TaskNotificationConsumer
 
 websocket_urlpatterns=[
-    path()
+    path('ws/notification-to-technicians/', TaskNotificationConsumer.as_asgi())
 ]
